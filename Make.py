a8a73ef5d98285f9c013195056001dd1989cbd8a
Erin Cole <Erin Cole>
2017-06-10 13:16:31 -0700

    enabled random numbers for shape coordinates

--- Make.py
+++ Make.py
@@ -2,19 +2,19 @@
 # This file creates all the needed components and containers for the project.
 
 from tkinter import *
+from Lib import random
 
-def mainWindow(h = 750, w = 750):
 
-    main = Tk()
-    w = Canvas(main, width = w, height = h)
-    w.pack()
 
-    w.mainloop()
-
-def circle(canvas, x, y, r):
+def circle(canvas, canvasWidth, canvasHeight, r):
+    x = random.randint(r+1, canvasWidth-r-1)
+    y = random.randint(r+1, canvasHeight-r-1)
     c = canvas.create_oval(x-r, y-r, x+r, y+r)
     return c
 
-def square(canvas, points):
+def square(canvas, canvasWidth, canvasHeight, side):
+    x = random.randint(side+1, canvasWidth-side-1)
+    y = random.randint(side+1, canvasHeight-side-1)
+    points = [x, y, x+side, y, x+side, y+side, x, y+side]
     s = canvas.create_polygon(points)
     return s
--- Proj2Driver.py
+++ Proj2Driver.py
@@ -4,32 +4,39 @@
 from tkinter import *
 from Lib import random
 import time
+import sys
 import Make
 
 
 # This is the main function for the project.  It creates a narrative in a loop 
-# with randomized function calls in order to hightlight the literal stupidity of# computers and the frustration that often gets evoked while using them.
+# with randomized function calls in order to hightlight the literal stupidity of
+# computers and the frustration that often gets evoked while using them.
 if __name__ == '__main__':
     shortPause = 2
     longPause = 5
 
-    xCoord = 100
-    yCoord = 100
+    canvasWidth = 750
+    canvasHeight = 750
     rad = 25
-
-    points = [200, 200, 250, 200, 250, 250, 200, 250]
-
-    #c = Canvas.make()
-    #c.mainloop()
+    side = 50
 
     main = Tk()
-    w = Canvas(main, width = 750, height = 750)
+    w = Canvas(main, width = canvasWidth, height = canvasHeight)
     w.pack()
 
-    Make.circle(w, xCoord, yCoord, rad)
     
-    Make.square(w, points)
+    c = Make.circle(w, canvasWidth, canvasHeight, rad)
+    
+    s = Make.square(w, canvasWidth, canvasHeight, side)
+    
+
+
+    x = random.randint(rad+1, canvasWidth-rad)
+    y = random.randint(rad+1, canvasHeight-rad)
+    sys.stdout.flush() 
+    time.sleep(longPause)
+    w.coords(c, x-rad, y-rad, x+rad, y+rad)
     #while 1:
+    w.mainloop()
 
     
-    w.mainloop()
